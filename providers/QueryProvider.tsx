3ee7d960e4232735b9d67e9d566df0074b278467
Samuel Franklin <samuelfranklinff@gmail.com>
2026-01-23 11:10:01 -0300

    feat: query provider

--- providers/QueryProvider.tsx
+++ providers/QueryProvider.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '../QueryClient';
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-    const [queryClient] = React.useState(
-        () =>
-            new QueryClient({
-                defaultOptions: {
-                    queries: {
-                        staleTime: 60 * 1000, // 1 minute
-                        refetchOnWindowFocus: false,
-                        retry: 1,
-                    },
-                },
-            })
-    );
-
     return (
         <QueryClientProvider client={queryClient}>
             {children}
